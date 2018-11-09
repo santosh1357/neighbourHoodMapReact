fdeebc45cbb51ca67d91c4a548f94fadd011449d
Santosh Prasad <santoshpsd13@gmail.com>
2018-11-10 02:04:21 +0530

    making marker image request https

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
   }
    makeMarkerIcon = (markerColor) =>  {
         var markerImage = new window.google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
           '|40|_|%E2%80%A2',
           new window.google.maps.Size(21, 34),
           new window.google.maps.Point(0, 0),
